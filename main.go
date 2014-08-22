abd271d92fa3d281e7d52fdf4644b00d04dc9c7a
aalekh <aalekh1993@rediffmail.com>
2014-08-22 08:04:27 +0530

    Added Mail content and meta-data

--- main.go
+++ main.go
@@ -32,4 +32,30 @@ func smtpServerCredential( smtpStruct smtpServerCredential ) string{
 		auth := smtp.PlainAuth("", smtpStruct.username, smtpStruct.password, smtpStruct.smtpServer)
 	}
 	return auth
-} 
+}
+
+type mailContent struct {
+	receipentName string
+	receipentEmail string
+	sendersName string
+	sendersEmail string
+	subject string
+	body string
+}
+
+func sendMail(structContent mailContent, authMail string){
+		header := make(map[string]string)
+		header["From"] = from.String()
+		header["To"] = to.String()
+		header["Subject"] = encodeRFC2047(title)
+		header["MIME-Version"] = "1.0"
+		header["Content-Type"] = "text/plain; charset=\"utf-8\""
+		header["Content-Transfer-Encoding"] = "base64"
+		 
+		message := ""
+		for meta, content := range header {
+		message += fmt.Sprintf("%s: %s\r\n", meta, content)
+		}
+		message += "\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+		
+}
